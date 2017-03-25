89da92d28ab4bd4b6e9ebdb296dadefc9dc9c910
Jonathan Lee <jsalinlee@gmail.com>
2017-03-25 13:37:02 -0700

    Added old RainbowRoad assignment for Coding Dojo.

--- RainbowRoad/ViewController.swift
+++ RainbowRoad/ViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var backgroundColors: [UIColor] = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.purple]
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +23,25 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Color", for: indexPath)
+        for index in 0 ... indexPath.row {
+            cell.backgroundColor = backgroundColors[index]
+        }
+        self.tableView.rowHeight = 120.0
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120.0
+    }
 }
 
